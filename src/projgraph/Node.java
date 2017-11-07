4f06e1ade8aff159a42aee7f4272fde0ba927fc3
stardisblue <stardisblue@gmail.com>
2017-11-07 20:55:23 +0100

    debuggin RDFRawParser

--- src/projgraph/Node.java
+++ src/projgraph/Node.java
@@ -1,89 +1,105 @@
 package projgraph;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class Node {
-	private int id; //index
-	public HashMap<Edge, HashSet<Node>> linkIn; //edge, nodeIn = object
-	public HashMap<Edge, HashSet<Node>> linkOut; //edge, nodeOut = subject
-	public HashMap<Node, ArrayList<Edge>> indexStructure = new HashMap<>(); //yolo
-	
-	Node(int id){
-		this.id = id;
-		linkIn = new HashMap<>();
-		linkOut = new HashMap<>();
-	}
-	
-	/**
-	 * 
-	 * @return id of Node
-	 */
-	public int getId() {
-		return id;
-	}
-	
-	/**
-	 * this is a object
-	 * nodeOut is a subject
-	 */
-	public void addLinkOut (Edge edge, Node nodeOut){
-		linkOut.computeIfAbsent(edge, k->new HashSet<Node>()).add(nodeOut);
-	}
-	
-	/**
-	 * this is a subject
-	 * @param edge is edge between the nodes
-	 * @param nodeIn is a object
-	 */
-	public void addLinkIn (Edge edge, Node nodeIn){
-		linkIn.computeIfAbsent(edge, k->new HashSet<Node>()).add(nodeIn);
-	}
-	
-	/**
-	 * Create an indexstructure from linkOut
-	 * @return indexed graph of node
-	 */
-	public void createIndex(){
-		for(Entry<Edge,HashSet<Node>> entry : linkOut.entrySet()){
-	        HashSet<Node> node = entry.getValue();
-	    	Edge edge = entry.getKey();
-	    	Iterator<Node> iterator = node.iterator();
-	    	while(iterator.hasNext()){
-	    		indexStructure.computeIfAbsent(iterator.next(), k-> new ArrayList<Edge>()).add(edge);
-	    	}
-	    }
-	}
-	
-	/**
-	 * 
-	 * @return indexed graph of node
-	 */
-	public HashMap<Node, ArrayList<Edge>> getIndexStructure(){
-		return indexStructure;
-	}
-	
-	/**
-	 * @param predicate Edge
-	 * @return Set of nodes
-	 */
-	public ArrayList<Node> requestIndexStructure(Edge predicate) {
-		ArrayList<Node> results = new ArrayList<>();
-		for (Entry<Node, ArrayList<Edge>> value : indexStructure.entrySet()) {
-			for(Edge e : value.getValue()) {
-				if(e.equals(predicate)) {
-					results.add(value.getKey());
-				}
-			}
-	    }
-		return results;
-	}
-	
-	/**
-	 * 
-	 * @return pretty print of nodeId
-	 */
-	public String toString() {
-		return "n" + id;
-	}
+    public HashMap<Edge, HashSet<Node>> linkIn; //edge, nodeIn = object
+    public HashMap<Edge, HashSet<Node>> linkOut; //edge, nodeOut = subject
+    public HashMap<Node, ArrayList<Edge>> indexStructure = new HashMap<>(); //yolo
+    private int id; //index
+
+    Node(int id) {
+        this.id = id;
+        linkIn = new HashMap<>();
+        linkOut = new HashMap<>();
+    }
+
+    /**
+     * @return id of Node
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * this is a object
+     * nodeOut is a subject
+     */
+    public void addLinkOut(Edge edge, Node nodeOut) {
+        linkOut.computeIfAbsent(edge, k -> new HashSet<Node>()).add(nodeOut);
+    }
+
+    /**
+     * this is a subject
+     *
+     * @param edge   is edge between the nodes
+     * @param nodeIn is a object
+     */
+    public void addLinkIn(Edge edge, Node nodeIn) {
+        linkIn.computeIfAbsent(edge, k -> new HashSet<Node>()).add(nodeIn);
+    }
+
+    /**
+     * Create an indexstructure from linkOut
+     *
+     * @return indexed graph of node
+     */
+    public void createIndex() {
+        for (Entry<Edge, HashSet<Node>> entry : linkOut.entrySet()) {
+            HashSet<Node> node = entry.getValue();
+            Edge edge = entry.getKey();
+            Iterator<Node> iterator = node.iterator();
+            while (iterator.hasNext()) {
+                indexStructure.computeIfAbsent(iterator.next(), k -> new ArrayList<Edge>()).add(edge);
+            }
+        }
+    }
+
+    /**
+     * @return indexed graph of node
+     */
+    public HashMap<Node, ArrayList<Edge>> getIndexStructure() {
+        return indexStructure;
+    }
+
+    /**
+     * @param predicate Edge
+     * @return Set of nodes
+     */
+    public ArrayList<Node> requestIndexStructure(Edge predicate) {
+        ArrayList<Node> results = new ArrayList<>();
+        for (Entry<Node, ArrayList<Edge>> value : indexStructure.entrySet()) {
+            for (Edge e : value.getValue()) {
+                if (e.equals(predicate)) {
+                    results.add(value.getKey());
+                }
+            }
+        }
+        return results;
+    }
+
+    /**
+     * @return pretty print of nodeId
+     */
+    public String toString() {
+        return "n" + id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Node)) return false;
+
+        Node n = (Node) obj;
+
+        return this == n || id == n.id;
+    }
 }
--- src/projgraph/RDFRawParser.java
+++ src/projgraph/RDFRawParser.java
@@ -1,99 +1,106 @@
 package projgraph;
 
-import java.io.FileNotFoundException;
+import org.oktmr.grafthug.query.Condition;
+import org.oktmr.grafthug.query.Query;
+import org.oktmr.grafthug.query.QueryParser;
+import org.oktmr.grafthug.query.exception.IncorrectConditionStructure;
+import org.oktmr.grafthug.query.exception.IncorrectPrefixStructure;
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.RDFHandlerBase;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map.Entry;
-
-import org.openrdf.model.Statement;
-import org.openrdf.rio.RDFFormat;
-import org.openrdf.rio.Rio;
-import org.openrdf.rio.helpers.RDFHandlerBase;
-
-import org.oktmr.grafthug.*;
-import org.oktmr.grafthug.query.Condition;
-import org.oktmr.grafthug.query.Query;
-import org.oktmr.grafthug.query.QueryParser;
 
 public final class RDFRawParser {
 
-	public static final DataStore ds = new DataStore();
-	public static final Dictionnaire dico = new Dictionnaire();
+    public static final DataStore ds = new DataStore();
+    public static final Dictionnaire dico = new Dictionnaire();
 
-	private static class RDFListener extends RDFHandlerBase {
+    public static void main(String args[]) throws FileNotFoundException {
 
-		@Override
-		public void handleStatement(Statement st) {
-			// System.out.println("\n" + st.getSubject() + "\t " + st.getPredicate() + "\t "
-			// + st.getObject());
-			// Transformer les valeurs en index (int)
-			int indexSubject = ds.add(st.getSubject().stringValue());
-			int indexPredicate = ds.add(st.getPredicate().stringValue());
-			int indexObject = ds.add(st.getObject().stringValue());
-			// Ajout des index au dictionnaire
-			dico.add(indexSubject, indexPredicate, indexObject);
-		}
+        Reader reader = new FileReader("University0_0.owl");
 
-	}
+        org.openrdf.rio.RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
+        rdfParser.setRDFHandler(new RDFListener());
+        try {
+            rdfParser.parse(reader, "");
+        } catch (IOException | RDFParseException | RDFHandlerException e) {
+            e.printStackTrace();
+        }
+        dico.index(); // Generate indexStructure
+        System.out.println("Number of nodes : " + dico.nodes.size());
+        System.out.println("Number of edges : " + dico.edges.size());
 
-	public static void main(String args[]) throws FileNotFoundException {
+        String q = " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+                + "PREFIX ub: <http://swat.cse.lehigh.edu/onto/univ-bench.owl#>" + " SELECT ?x "
+                + "WHERE {?x rdf:type ub:Subj18Student .  ?x rdf:type ub:GraduateStudent . ?x rdf:type ub:TeachingAssistant }";
+        Query query = null;
+        try {
+            query = QueryParser.parse(q);
+        } catch (IncorrectPrefixStructure | IncorrectConditionStructure e) {
+            e.printStackTrace();
+        }
+        System.out.println(query);
+        System.out.println(Arrays.asList(query.getVariables()));
+        System.out.println(Arrays.asList(query.getConditions()));
+        System.out.println(Arrays.asList(query.getPrefixes()));
+        ArrayList<Condition> conds = query.getConditions();
 
-		Reader reader = new FileReader("University0_0.owl");
+        // fin du pretraitement
+        ArrayList<Node> results = new ArrayList<>();
+        for (Condition cond : conds) {
+            Edge predicate = dico.edges.get(ds.valueIndexes.get(cond.getPredicate().stringValue())); // Return edge
+            // (predicate) of a
+            // condition
+            Node indexNode = dico.nodes.get(ds.valueIndexes.get(cond.getObject().stringValue())); // Return node
+            // (object) of a condition
+            ArrayList<Node> subjects = indexNode.requestIndexStructure(predicate);
 
-		org.openrdf.rio.RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
-		rdfParser.setRDFHandler(new RDFListener());
-		try {
-			rdfParser.parse(reader, "");
-			dico.index(); // Generate indexStructure
-			System.out.println("Number of nodes : " + dico.nodes.size());
-			System.out.println("Number of edges : " + dico.edges.size());
-			
-			String q = " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-					+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-					+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
-					+ "PREFIX ub: <http://swat.cse.lehigh.edu/onto/univ-bench.owl#>" + " SELECT ?x "
-					+ "WHERE {?x rdf:type ub:Subj18Student .  ?x rdf:type ub:GraduateStudent . ?x rdf:type ub:TeachingAssistant }";
-			Query query = QueryParser.parse(q);
-			System.out.println(query);
-			System.out.println(Arrays.asList(query.getVariables()));
-			System.out.println(Arrays.asList(query.getConditions()));
-			System.out.println(Arrays.asList(query.getPrefixes()));
-			ArrayList<Condition> conds = query.getConditions();
-			ArrayList<Node> results = new ArrayList<>();
-			for (Condition cond : conds) {
-				ArrayList<Node> subjects = new ArrayList<>();
-				Edge predicate = dico.addEdge(ds.add(cond.getPredicate().getName())); // Return edge(predicate) of a condition
-				Node indexNode = dico.addNode(ds.add(cond.getObject().getName())); // Return node(object) of a condition
-				subjects = indexNode.requestIndexStructure(predicate);
-				if (results.isEmpty()) {
-					results.addAll(subjects);
-				} else {
-					for (Node result : results) {
-						if (!subjects.contains(result)) {
-							results.remove(result);
-						}
-					}
-				}
-			}
-			ArrayList<String> finalResults = new ArrayList<>();
-			for(Node result : results) {
-				finalResults.add(ds.getValue(result.getId()));
-			}
-			System.out.println("Resultat de la requete : " + finalResults);
-		} catch (Exception e) {
+            if (results.isEmpty()) {
+                results.addAll(subjects);
+            } else {
+                results.removeIf(result -> !subjects.contains(result));
+            }
+        }
 
-		}
+        ArrayList<String> finalResults = new ArrayList<>();
+        for (Node result : results) {
+            finalResults.add(ds.getValue(result.getId()));
+        }
+        System.out.println("Resultat de la requete : " + finalResults);
 
-		try {
-			reader.close();
-		} catch (IOException e) {
-		}
 
-	}
+        try {
+            reader.close();
+        } catch (IOException e) {
+        }
+
+    }
+
+    private static class RDFListener extends RDFHandlerBase {
+
+        @Override
+        public void handleStatement(Statement st) {
+            // System.out.println("\n" + st.getSubject() + "\t " + st.getPredicate() + "\t "
+            // + st.getObject());
+            // Transformer les valeurs en index (int)
+            int indexSubject = ds.add(st.getSubject().stringValue());
+            int indexPredicate = ds.add(st.getPredicate().stringValue());
+            int indexObject = ds.add(st.getObject().stringValue());
+            // Ajout des index au dictionnaire
+            dico.add(indexSubject, indexPredicate, indexObject);
+        }
+
+    }
 
 }
